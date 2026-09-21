26d995e66277de6a719465c42e8fb6214e322dfc
onLaait <gakrs0817@gmail.com>
2026-09-21 23:56:35 +0900

    Update

--- src/main/java/io/github/onlaait/warudodownloader/ClientBiome.java
+++ src/main/java/io/github/onlaait/warudodownloader/ClientBiome.java
@@ -9,7 +9,7 @@ import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.biome.BiomeSpecialEffects;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 
-public class ClientBiome {
+public final class ClientBiome {
 
     public static final Codec<Biome> DIRECT_CODEC = RecordCodecBuilder.create(
             i -> i.group(
